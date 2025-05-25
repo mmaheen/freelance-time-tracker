c2824581a9327b185f234d20e8b6c934a21d2959
mmaheen <mushrifmaheen@gmail.com>
2025-05-25 19:29:49 +0600

    API routes

---
+++ app/Http/Controllers/API/UserAuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserAuthController extends Controller
+{
+    //
+    public function register(Request $request)
+    {
+        // Logic for user registration
+        
+    }
+
+    public function login(Request $request)
+    {
+        // Logic for user login
+    }
+}
