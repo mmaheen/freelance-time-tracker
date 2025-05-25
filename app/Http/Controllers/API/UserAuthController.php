433588e3198816cb06b62c09e8d9f191c26ee80a
mmaheen <mushrifmaheen@gmail.com>
2025-05-25 19:39:11 +0600

    API register without validation

--- app/Http/Controllers/API/UserAuthController.php
+++ app/Http/Controllers/API/UserAuthController.php
@@ -2,16 +2,26 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UserAuthController extends Controller
 {
     //
     public function register(Request $request)
     {
-        // Logic for user registration
-        
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+
     }
 
     public function login(Request $request)
