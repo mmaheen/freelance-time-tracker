1054d6e13a31322a3234bc394b202dfd92ea10d6
mmaheen <mushrifmaheen@gmail.com>
2025-05-25 19:53:43 +0600

    API register with token

--- app/Http/Controllers/API/UserAuthController.php
+++ app/Http/Controllers/API/UserAuthController.php
@@ -5,12 +5,24 @@ namespace App\Http\Controllers\API;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class UserAuthController extends Controller
 {
     //
     public function register(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
